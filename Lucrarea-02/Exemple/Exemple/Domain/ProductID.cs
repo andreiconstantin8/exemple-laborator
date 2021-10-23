882306b0d15d2c73bc5b8a4f4a27b3df590dfbbd
andreiconstantin8 <73140217+andreiconstantin8@users.noreply.github.com>
2021-10-23 14:55:21 +0300

    Add files via upload

---
+++ Lucrarea-02/Exemple/Exemple/Domain/ProductID.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple.Domain
+{
+    public record ProductID
+    {
+        public decimal Value { get; }
+
+        public ProductID(decimal value)
+        {
+            if (IsValid(value))
+            {
+                Value = value;
+            }
+            else
+            {
+                throw new InvalidProductIDException($"{value:0.##} is an invalid product ID value.");
+            }
+        }
+
+        public ProductID Round()
+        {
+            var roundedValue = Math.Round(Value);
+            return new ProductID(roundedValue);
+        }
+
+        public override string ToString()
+        {
+            return $"{Value:0.##}";
+        }
+
+        public static bool TryParseProductID(string productIDString, out ProductID productID)
+        {
+            bool isValid = false;
+            productID = null;
+            if(decimal.TryParse(productIDString, out decimal numericProductID))
+            {
+                if(IsValid(numericProductID))
+                {
+                    isValid = true;
+                    productID = new(numericProductID);
+                }
+            }
+            return isValid;
+        }
+
+        private static bool IsValid(decimal numericProductID) => numericProductID > 0 && numericProductID <= 10;
+    }
+}
